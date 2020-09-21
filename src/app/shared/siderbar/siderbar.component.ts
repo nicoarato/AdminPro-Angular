5848581db551cd8c1de042b05599c34671e13396
Nico Arato <nico.arato@gmail.com>
2020-09-21 02:03:12 -0300

    first commit

---
+++ src/app/shared/siderbar/siderbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-siderbar',
+  templateUrl: './siderbar.component.html',
+  styles: [
+  ]
+})
+export class SiderbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
